d54977ddf7bcdb270326bbbd40b967246988b34b
Takaaki ABE <abnoumaru@gmail.com>
2024-09-15 02:31:35 +0900

    techブログの切り出しやめた

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,8 +3,8 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = (await getCollection('blog')).filter(
-		item => item.data.isTech === true
+	const posts = (await getCollection('blog')).sort(
+		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
 	);
 	return rss({
 		title: SITE_TITLE,
@@ -12,7 +12,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/tech/${post.slug}/`,
+			link: `/blog/${post.slug}/`,
 		})),
 	});
 }
