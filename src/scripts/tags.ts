bf3eee7f28faa5ada4a2a5d845286bd431879d46
abnoumaru <abnoumaru@gmail.com>
2024-09-22 00:05:07 +0900

    Feature/20240920 2 (#14)
    * uecのlinear-gradientに色を追加
    
    * フォーマットを修正
    
    * uecのlinear-gradientの勾配を変更
    
    * フッターを修正
    
    * 最近聴いた曲をLast.fmから取得する
    
    * 同一レポで楽曲取得は諦めた
    
    * Perttierを設定してコードを整形
    
    * npm audit fixを実行
    
    * eslintを追加
    
    * pubsから不要なsectionを削除

--- src/scripts/tags.ts
+++ src/scripts/tags.ts
@@ -1,13 +1,13 @@
-import { getCollection } from 'astro:content';
+import { getCollection } from "astro:content";
 
 export async function createUniqueTagsList(): Promise<string[]> {
-    const posts = await getCollection('blog');
+  const posts = await getCollection("blog");
 
-    const allTags = posts.reduce<string[]>((acc, item) => {
-        return acc.concat(item.data.tags);
-    }, []);
+  const allTags = posts.reduce<string[]>((acc, item) => {
+    return acc.concat(item.data.tags);
+  }, []);
 
-    const uniqueTags = Array.from(new Set(allTags));
+  const uniqueTags = Array.from(new Set(allTags));
 
-    return uniqueTags;
+  return uniqueTags;
 }
