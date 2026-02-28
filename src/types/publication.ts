9b46232b234612eab5b953cc7c159e89f7ee0765
Takaaki Abe <abnoumaru@gmail.com>
2026-02-28 19:44:36 +0900

    refactor: remove outputs page and move external links to links page

--- src/types/publication.ts
+++
@@ -1,8 +0,0 @@
-export interface Publication {
-  title: string;
-  url: string;
-  pubDate: Date;
-  source: "note" | "zenn" | "speakerdeck";
-}
-
-export type PublicationSource = Publication["source"];
